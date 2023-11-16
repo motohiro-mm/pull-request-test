450455a74176ca0b5fbd711bbf4ad6b93af71564
motohiro-mm <polarbear.dx@gmail.com>
2023-11-16 21:13:37 +0900

    Revert "irb.rbを反対に"
    This reverts commit 888471308720d00ac00b2f8384c69ad9d734f911.

--- irb.rb
+++ irb.rb
@@ -1,12 +1,12 @@
 foods = ['ピーマン','トマト','セロリ']
 count = 0
 foods.each do |food|
-  print "#{food}はお嫌いですか？ => "
+  print "#{food}はお好きですか？ => "
   answer = [ 'はい', 'いいえ', 'その他' ].sample
   puts answer
 
   count += 1
-  redo if answer != 'いいえ' && count < 3
+  redo if answer != 'はい' && count < 3
 
   count = 0
 end
